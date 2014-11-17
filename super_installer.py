b864a62564e88cb96c3d93a246c340b956b1ddb3
Yask Srivastava <yask123@gmail.com>
2014-11-17 20:53:03 +0530

    Fixed major path bug

--- super_installer.py
+++ super_installer.py
@@ -4,8 +4,9 @@ os.system('pip install -r requirements.txt')
 current_path = os.getcwd()
 
 os.system('cd ~')
-os.system('mkdir .music_downloader')
-os.system('cp '+current_path+'/music_downloader.py /.music_downloader')
+prof_path = os.getenv("HOME")
+os.system('mkdir ~/.music_downloader')
+os.system('cp '+current_path+'/music_downloader.py ~/.music_downloader')
 
-with open ('.profile','a') as f:
+with open ('prof_path/.profile','a') as f:
 	f.write('PATH="$PATH:~/.music_downloader')
