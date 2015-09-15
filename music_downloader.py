cd5d30dd96e8ca9f92290fdaf46b1f324ea76af5
Yask Srivastava <yask123@gmail.com>
2015-09-15 23:00:24 +0530

    Updated to use BeautifulSoup, Bug fixes

--- music_downloader.py
+++ music_downloader.py
@@ -1,38 +1,29 @@
-#!/usr/bin/env python
+# /usr/local/bin/python
 
 import os
 import glob
-
+from bs4 import BeautifulSoup
 import urllib2
-intro_art = """
- \ \   / /      | |  ( )     |  \/  |         (_)      |  __ \                    | |               | |          
-  \ \_/ /_ _ ___| | _|/ ___  | \  / |_   _ ___ _  ___  | |  | | _____      ___ __ | | ___   __ _  __| | ___ _ __ 
-   \   / _` / __| |/ / / __| | |\/| | | | / __| |/ __| | |  | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__|
-    | | (_| \__ \   <  \__ \ | |  | | |_| \__ \ | (__  | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   
-    |_|\__,_|___/_|\_\ |___/ |_|  |_|\__,_|___/_|\___| |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|  
-"""
-print (intro_art)
-choice = 'Y'
-while(choice == 'Y'):
-	try :
-		search = 'love story taylor' # Test Case 
-		search = testVar = raw_input("Enter Song Name\n")
 
-		search = search.replace(' ','%20')
-		print('Making a Query Request! ')
-		response = urllib2.urlopen('https://www.youtube.com/results?search_query='+search)
-		html = response.read()
-		a =html.find('<h3 class="yt-lockup-title"><a href="/watch?')
-		raw_link= (html[a+43:a+57]) # May change when Youtube Website may get updated in the future.
-		proper_linl = 'https://www.youtube.com/watch'+raw_link
+search = 'love story taylor' #I love you Taylor swift!
+search = raw_input('Enter songname/ lyrics/ artist.. or whatever ')
+search = search.replace(' ','%20')
 
-		#youtube-dl --extract-audio --audio-format mp3
-		#command='youtube-dl -t --format bestaudio '+proper_linl
-		command = 'youtube-dl --extract-audio --audio-format mp3 '+proper_linl
-		print ('Processed Querying , Starting Phase 2')
-		os.system(command)
+print('Making a Query Request! ')
+response = urllib2.urlopen('https://www.youtube.com/results?search_query='+search)
+html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
+for link in soup.find_all('a'):
+    if '/watch?v=' in link.get('href'):
+    	print(link.get('href'))
+    	# May change when Youtube Website may get updated in the future.
+    	proper_linl = link.get('href')
+    	break
+ 
+proper_linl =  'http://www.youtube.com/'+proper_linl
+command = 'youtube-dl --extract-audio --audio-format mp3 '+proper_linl
+print ('Processed Querying , Starting Phase 2')
+os.system(command)
 
-		choice = raw_input('Download Another music Y/N ?')
-	except :
-		print ('Sorry , An error eccored , please report the Bug')	
+
 
