8d3437de5267572033336021dae2b97a86364a29
Nishant Arora <nishantarora@google.com>
2015-09-17 11:08:34 +0530

    Adding a better installer script and changing the music downloader script.

--- music_downloader.py
+++ music_downloader.py
@@ -6,11 +6,18 @@ from bs4 import BeautifulSoup
 import urllib2
 from urllib import quote_plus as qp
 
-search = 'love story taylor' #I love you Taylor swift!
-search = raw_input('Enter songname/ lyrics/ artist.. or whatever ')
+# High Quality Songs, yeah baby!
+DEFAULT_AUDIO_QUALITY = '320K'
+
+search = ''
+# We do not want to except empty inputs :)
+while search == '':
+  search = raw_input('Enter songname/ lyrics/ artist.. or whatever ')
 search = qp(search)
 
 print('Making a Query Request! ')
+
+# Magic happens here.
 response = urllib2.urlopen('https://www.youtube.com/results?search_query='+search)
 html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
@@ -18,12 +25,16 @@ for link in soup.find_all('a'):
     if '/watch?v=' in link.get('href'):
     	print(link.get('href'))
     	# May change when Youtube Website may get updated in the future.
-    	proper_linl = link.get('href')
+    	video_link = link.get('href')
     	break
- 
-proper_linl =  'http://www.youtube.com/'+proper_linl
-command = 'youtube-dl --extract-audio --audio-format mp3 '+proper_linl
-print ('Processed Querying , Starting Phase 2')
+
+# Links are relative on page, making them absolute.
+video_link =  'http://www.youtube.com/'+proper_linl
+command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +
+           DEFAULT_AUDIO_QUALITY + ' ' +proper_linl)
+
+# Youtube-dl is a proof that god exists.
+print ('Downloading...')
 os.system(command)
 
 
