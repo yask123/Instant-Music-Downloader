6ef08e3c65e2d487a43e6ad74cab5a95ad9202bc
Aaron Ouellette <aouellette2016@gmail.com>
2015-09-23 09:47:43 -0400

    print video title and prompt for download

--- music_downloader.py
+++ music_downloader.py
@@ -28,10 +28,15 @@ html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
 for link in soup.find_all('a'):
     if '/watch?v=' in link.get('href'):
-    	print(link.get('href'))
     	# May change when Youtube gets updated in the future.
     	video_link = link.get('href')
     	break
+
+title = title = soup.find("a", "yt-uix-tile-link").text
+print("Found: " + title)
+prompt = raw_input("Download song (y/n)? ")
+if prompt != "y":
+    sys.exit()
 
 # Links are relative on page, making them absolute.
 video_link = 'http://www.youtube.com/' + video_link
