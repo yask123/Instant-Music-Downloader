9456640b692b7d68ff874ab796b4f87563e85391
aviaryan <avi.aryan123@gmail.com>
2015-09-24 23:17:40 +0530

    give warning if path length going to exceed 1024

--- install.bat
+++ install.bat
@@ -12,11 +12,16 @@ copy ".\music_downloader.py" "%UserProfile%\music_downloader\music_downloader.py
 
 echo. ";%PATH%;" | findstr /C:";%UserProfile%\music_downloader;" 1>nul
 if errorlevel 1 (
-	@setx PATH "%PATH%;%UserProfile%\music_downloader" /m
+    set svar="%PATH%;%UserProfile%\music_downloader;;"
+    goto strlen
+    :pathok
+    @setx PATH "%PATH%;%UserProfile%\music_downloader" /m
+    goto end
 ) ELSE (
 	echo Already exists in PATH
 )
 
+:end
 set /p tempvar="SUCCESS. Press Enter to continue"
 exit /B
 
@@ -33,4 +38,25 @@ rem http://stackoverflow.com/a/11995662/2295672
     ) else (
         echo Failure: Try running as administrator
     	pause >nul
+        exit /B
+    )
+
+rem http://stackoverflow.com/a/5841587/2295672
+
+:strlen
+    rem setlocal EnableDelayedExpansion for changing var and using it in loop
+    setlocal EnableDelayedExpansion
+    set "s=!%svar%!#"
+    set "len=0"
+    for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+        if "!s:~%%P,1!" NEQ "" ( 
+            set /a "len+=%%P"
+            set "s=!s:~%%P!"
+        )
+    )
+    if %len% GEQ 1024 (
+        set /p tempvar="PATH is more than 1024 characters. This can't be handled by the script. You will have to add that manually."
+        exit /b
+    ) else (
+        goto pathok
     )
