403c64ad4860e457d39a0a5bb8d2c8d4f8f6524c
Yask Srivastava <yask123@gmail.com>
2015-10-04 03:34:01 +0530

    package created

---
+++ instantmusic-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='instantmusic',
+      version='0.1',
+      description='Instantly download any song! Without knowing the name of the song!!!!',
+      url='https://github.com/yask123/Instant-Music-Downloader',
+      author='Yask Srivastava',
+      author_email='yask123@gmail.com',
+      license='MIT',
+      packages=['instantmusic'],
+      scripts=['bin/instantmusic'],
+      zip_safe=False)
